fbbee635820dcd8af2c808da4622054a45be015c
Mohamed Rizwan <mynameisrizwan35@gmail.com>
2023-11-14 15:48:46 +0530

    minor changes

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,5 +1,5 @@
 import Layout from "@/components/layout";
-import ProductForm from "@/components/productForm";
+import ProductForm from "@/components/ProductForm";
 
 
 export default function NewProduct() {
